fb2998b3872a12ad96fbf6906a03f0b7f28173a2
AnastasiiaDia <anastasiia.iv@gmail.com>
2023-10-04 15:12:16 +0200

    project finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Container } from './App.styled';
+import ImageGallery from './ImageGallery/ImageGallery';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    searchText: '',
+  };
+
+  handleSearch = searchText => {
+    this.setState({ searchText });
+  };
+  render() {
+    return (
+      <Container>
+        <Searchbar handleSearch={this.handleSearch}></Searchbar>
+        <ImageGallery searchText={this.state.searchText} />
+      </Container>
+    );
+  }
+}
